24518fc5fb800107296ee7c1bb632d1e424f0ea5
ahmed <ahmedlearn01@gmail.com>
2022-01-23 07:23:40 -0800

    create login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,15 @@
 import Login from "./Login";
+import {useState} from 'react';
 
 function App() {
-  return <Login />
+  const [id, setId] = useState('');
+
+  return( 
+    <>
+     {id}
+      <Login onSubmitId = {setId}/>
+    </>
+  )
 }
 
 export default App;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,22 @@
-import { Container, Form } from "react-bootstrap";
+import { Container, Form, Button} from "react-bootstrap";
 import { useRef } from "react";
 
-const Login = () => {
+const Login = ({onSubmitId}) => {
     const idRef = useRef();
+    const onSubmitForm = (e)=>{
+        e.preventDefault();
 
+        onSubmitId(idRef.current.value);
+    }
   return(
       <Container className="align-items-center d-flex" style={{height: '100vh'}}>
-          <Form>
+          <Form className="w-100" onSubmit={onSubmitForm}>
               <Form.Group>
                   <Form.Label>Enter Your ID</Form.Label>
                   <Form.Control type="text" ref={idRef} required />
               </Form.Group>
+              <Button type="submit" className="me-2 mt-2">Login</Button>
+              <Button variant="secondary" className="mt-2">Create New Id</Button>
           </Form>
       </Container>
   )
